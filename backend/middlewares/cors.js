394a7e01370c9500af44b988207e288d0c95cf7c
Mary O <marymary-o@ya.ru>
2023-08-17 15:04:13 +0300

    15 sprint/pre-test2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,13 +5,13 @@ const allowedCors = [
   'http://api.marymary.students.nomoreparties.co',
   'http://localhost:3000',
   'http://localhost:3001',
-  'http://127.0.0.1:3000'
+  'http://127.0.0.1:3000',
 ];
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
